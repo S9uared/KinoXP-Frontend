aca91e97fc1f8b6220fe51358962221b4ba56e6b
laura ramgil <lauraramgil@hotmail.com>
2023-10-10 14:31:05 +0200

    Implement program anonymois showings

--- index.js
+++ index.js
@@ -1,97 +1,96 @@
-import "./navigo_EditedByLars.js"  //Will create the global Navigo, with a few changes, object used below
-import {setActiveLink, loadHtml, renderHtml} from "./utils.js"
-import { initMovieSeats } from "./pages/showSeats/seats.js"
+import "./navigo_EditedByLars.js"; //Will create the global Navigo, with a few changes, object used below
+import { setActiveLink, loadHtml, renderHtml } from "./utils.js";
+import { initMovieSeats } from "./pages/showSeats/seats.js";
+import { initProgram } from "./pages/program/program.js";
 
-
-
-    window.addEventListener("load", async () => {
-        
-        //Add html pages in this format for client redirect  
-        //const templateCars = await loadHtml("./pages/cars/cars.html")
-        const templateFrontPage = await loadHtml("./pages/frontpage/frontpage.html")
-        const templateProgram = await loadHtml("./pages/program/program.html")
-        const templateMovieSchedule = await loadHtml("./pages/showMovieSchedule/schedule.html")
-        const templateMovieSeats = await loadHtml("./pages/showSeats/seats.html")
-        const templateBooking = await loadHtml("./pages/addBooking/booking.html")
-        const templateMyTickets = await loadHtml("./pages/findTickets/findTickets.html")
-        const templateLogin = await loadHtml("./pages/login/login.html")
-        const templateNotFound = await loadHtml("./pages/notFound/notFound.html")
-         //If token existed, for example after a refresh, set UI accordingly
-  const token = localStorage.getItem("token")
+window.addEventListener("load", async () => {
+  //Add html pages in this format for client redirect
+  //const templateCars = await loadHtml("./pages/cars/cars.html")
+  const templateFrontPage = await loadHtml("./pages/frontpage/frontpage.html");
+  const templateProgram = await loadHtml("./pages/program/program.html");
+  const templateMovieSchedule = await loadHtml(
+    "./pages/showMovieSchedule/schedule.html"
+  );
+  const templateMovieSeats = await loadHtml("./pages/showSeats/seats.html");
+  const templateBooking = await loadHtml("./pages/addBooking/booking.html");
+  const templateMyTickets = await loadHtml(
+    "./pages/findTickets/findTickets.html"
+  );
+  const templateLogin = await loadHtml("./pages/login/login.html");
+  const templateNotFound = await loadHtml("./pages/notFound/notFound.html");
+  //If token existed, for example after a refresh, set UI accordingly
+  const token = localStorage.getItem("token");
   //toggleLoginStatus(token)
 
- const router = new Navigo("/", { hash: true });
+  const router = new Navigo("/", { hash: true });
   //Not especially nice, BUT MEANT to simplify things. Make the router global so it can be accessed from all js-files
-  window.router = router
+  window.router = router;
 
   router
     .hooks({
       before(done, match) {
-        setActiveLink("menu", match.url)
-        done()
-      }
+        setActiveLink("menu", match.url);
+        done();
+      },
     })
     .on({
       //For very simple "templates", you can just insert your HTML directly like below
       //Add ',' between each url redirect.
-    //   "/": () => document.getElementById("content").innerHTML = `
-    //     <h2>Home</h2>
-    //     <img style="width:50%;max-width:600px;margin-top:1em;" src="./images/cars.png">
-    //     <p style='margin-top:1em;font-size: 1.5em;color:darkgray;'>
-    //       Car's 'R' Us - Created, as a help to make GREAT fullstack developers <span style='font-size:2em;'>&#128516;</span>
-    //     </p>
-    //  `,
-        
-        "/": () => {
-            renderHtml(templateFrontPage, "content")
-            //initFrontPage()
-        },
+      //   "/": () => document.getElementById("content").innerHTML = `
+      //     <h2>Home</h2>
+      //     <img style="width:50%;max-width:600px;margin-top:1em;" src="./images/cars.png">
+      //     <p style='margin-top:1em;font-size: 1.5em;color:darkgray;'>
+      //       Car's 'R' Us - Created, as a help to make GREAT fullstack developers <span style='font-size:2em;'>&#128516;</span>
+      //     </p>
+      //  `,
 
-        "/program": () => {
-            renderHtml(templateProgram, "content")
-            //initProgram()
-        },
+      "/": () => {
+        renderHtml(templateFrontPage, "content");
+        //initFrontPage()
+      },
 
-        "/movie/time" : () => {
-            renderHtml(templateMovieSchedule, "content")
-           // initMovieTimes()
-        },
+      "/program": () => {
+        renderHtml(templateProgram, "content");
+        initProgram();
+      },
 
-        "/movie/seats" : () => {
-            renderHtml(templateMovieSeats, "content")
-            initMovieSeats()
-        },
+      "/movie/time": () => {
+        renderHtml(templateMovieSchedule, "content");
+        // initMovieTimes()
+      },
 
-        "/movie/booking" : () => {
-            renderHtml(templateBooking, "content")
-           // initBooking()
-        },
+      "/movie/seats": () => {
+        renderHtml(templateMovieSeats, "content");
+        initMovieSeats();
+      },
 
-        "/mytickets" : () => {
-            renderHtml(templateMyTickets, "content")
-           // initMyTickets()
-        },
+      "/movie/booking": () => {
+        renderHtml(templateBooking, "content");
+        // initBooking()
+      },
 
-        "/employee/login": () => {
-            renderHtml(templateLogin, "content")
-           // initLogin()
-        },
+      "/mytickets": () => {
+        renderHtml(templateMyTickets, "content");
+        // initMyTickets()
+      },
 
-        "/employee/program" : () => {
-            renderHtml(templateEmpProgram, "content")
-            //initEmpProgram()
-        },
+      "/employee/login": () => {
+        renderHtml(templateLogin, "content");
+        // initLogin()
+      },
 
-        "/employee/booking" : () => {
-            renderHtml(templateEmpBooking, "content")
-            //initEmpBooking()
-        },
+      "/employee/program": () => {
+        renderHtml(templateEmpProgram, "content");
+        //initEmpProgram()
+      },
+
+      "/employee/booking": () => {
+        renderHtml(templateEmpBooking, "content");
+        //initEmpBooking()
+      },
     })
-      .notFound(() => {
-        renderHtml(templateNotFound, "content")
-      })
-      .resolve()
-    
-
-
-    });
+    .notFound(() => {
+      renderHtml(templateNotFound, "content");
+    })
+    .resolve();
+});
---
+++ pages/program/program.js
@@ -0,0 +1,102 @@
+import { API_URL } from "../../settings.js";
+const URL = API_URL + "/showings";
+import {
+  makeOptions,
+  sanitizeStringWithTableRows,
+  handleHttpErrors,
+} from "../../utils.js";
+
+export async function initProgram() {
+  initShowings();
+  document.getElementById("showings-by-date-result").innerText = "";
+  document
+    .getElementById("input-showing-date")
+    .addEventListener("change", findShowingsByDate);
+}
+
+async function findShowingsByDate() {
+  const date = document.getElementById("input-showing-date").value;
+
+  try {
+    const response = await fetch(
+      URL + "/date/" + date,
+      makeOptions("GET", null, false)
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const showings = await response.json();
+
+    if (!Array.isArray(showings)) {
+      throw new Error("Response data is not an array");
+    }
+
+    const cards = showings
+      .map(
+        (showing) => `
+            <div class="showing-card">
+                <img 
+                src="${showing.movie.Poster}" 
+                loading="lazy"
+                class="showing-pic"
+                />
+                <div class="showing-card-content">
+                    <h6 class="showing-title">${showing.movie.Title}</h6>
+                    <p class="showing-runtime">Runtime: ${showing.movie.Runtime}</p>
+                    <p class="showing-runtime">Date: ${showing.date}</p>
+                    <p class="showing-runtime">Time: ${showing.time}</p>
+                </div>
+            </div>
+        `
+      )
+      .join("");
+
+    document.getElementById("showings-result").innerHTML = cards;
+  } catch (error) {
+    console.error("An error occurred:", error);
+    // Handle the error appropriately, e.g., display an error message to the user.
+  }
+}
+
+async function initShowings() {
+  try {
+    const response = await fetch(URL, makeOptions("GET", null, false));
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const showings = await response.json();
+
+    if (!Array.isArray(showings)) {
+      throw new Error("Response data is not an array");
+    }
+
+    const cards = showings
+      .map(
+        (showing) => `
+            <div class="showing-card">
+                <img 
+                src="${showing.movie.Poster}" 
+                loading="lazy"
+                class="showing-pic"
+                />
+                <div class="showing-card-content">
+                    <h6 class="showing-title">${showing.movie.Title}</h6>
+                    <p class="showing-runtime">Runtime: ${showing.movie.Runtime}</p>
+                    <p class="showing-runtime">Date: ${showing.date}</p>
+                    <p class="showing-runtime">Time: ${showing.time}</p>
+                </div>
+            </div>
+        `
+      )
+      .join("");
+
+    document.getElementById("showings-result").innerHTML = cards;
+  } catch (error) {
+    console.error("An error occurred:", error);
+    // Handle the error appropriately, e.g., display an error message to the user.
+  }
+}
