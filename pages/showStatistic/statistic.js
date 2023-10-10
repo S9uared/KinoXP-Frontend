f6edb4728339e350765189c2080d4929742c8a8b
simo75m1 <simo75m1@stud.kea.dk>
2023-10-10 22:30:14 +0200

    movie-tabledata function adjusted

--- pages/showStatistic/statistic.js
+++ pages/showStatistic/statistic.js
@@ -20,17 +20,26 @@ async function fetchStatistics(){
 }
 
 function createMovieDivs(statData){
+    let statRows;
     if(statData instanceof Array){
-        const statRows = statData.map(stat => {
+        statRows = statData.map(stat => {
             `<tr>
             <td>${stat.movieId}</td>
             <td>${stat.movieName}</td>
             <td>${stat.date}</td>
             <td>${stat.totalReservations}</td>
+            <td><button id="graph-${stat.movieId}">...</button></td>
             </tr>`
         }).join("");
     } else if (movieData instanceof Object){
-
+        statRows = 
+        `<tr>
+        <td>${statData.movieId}</td>
+        <td>${statData.movieName}</td>
+        <td>${statData.date}</td>
+        <td>${statData.totalReservations}</td>
+        <td><button id="graph-${statData.movieId}">...</button></td>
+        </tr>`
     }
 
 }
