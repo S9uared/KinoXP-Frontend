88199c7b96f02275e33d88bf077b76741b48dc9c
simo75m1 <simo75m1@stud.kea.dk>
2023-10-12 12:25:39 +0200

    wrong input handling setup

--- pages/manageTheaters/theater.js
+++ pages/manageTheaters/theater.js
@@ -1,14 +1,18 @@
 import {API_URL} from "../../settings.js";
 import {handleHttpErrors, makeOptions, sanitizeStringWithTableRows,} from "../../utils.js";
 const url = API_URL+"/theaters"
-
+const theaterIds = [];
 export function initTheaters(){
     fetchTheaters();
-    document.getElementById("create-theater-btn").addEventListener("click", addTheater)
+    document.getElementById("create-theater-submit").addEventListener("click", addTheater)
+    document.getElementById("create-theater-btn").addEventListener("click", function (){
+        document.getElementById("modal").style.display = "block"
+        console.log(theaterIds);
+    })
 }
 
+
 async function fetchTheaters(){
-    let theaterIds = [];
     try{
         const theaters = await fetch(url, makeOptions("GET", null, true)).then(handleHttpErrors)
         const theaterDivs = theaters.map(theater => {
@@ -60,14 +64,34 @@ async function editTheater(event){
 }
 
 async function addTheater(){
+    document.getElementById("create-theater-fail").innerText = ""
     const newTheater = {
-        id : document.getElementById("create-theater-id").value,
+        id : parseInt(document.getElementById("create-theater-id").value),
         rows : document.getElementById("create-theater-rows").value,
         seatsPerRow : document.getElementById("create-theater-seats").value
     }
+    if(theaterIds.includes(newTheater.id, 0)){
+        clearCreateInputFields()
+        document.getElementById("create-theater-fail").innerText = "This theater id is already in use"
+        return;
+    }
+    if(newTheater.id == 0){
+        clearCreateInputFields()
+        document.getElementById("create-theater-fail").innerText = "Id can not be below 1"
+        return;
+    }
+    if((newTheater.rows == 0 && newTheater.seatsPerRow == 0)){
+        clearCreateInputFields()
+        document.getElementById("create-theater-fail").innerText = "Please add rows and or seats to the theater"
+        return;
+    }
+    clearCreateInputFields()
+    const postResponse = await fetch(url, makeOptions("POST", newTheater, true)).then(handleHttpErrors)
+    document.getElementById("modal").style.display = "none";
+    console.log(postResponse)
+}
+function clearCreateInputFields(){
     document.getElementById("create-theater-id").value = "";
     document.getElementById("create-theater-rows").value = "";
     document.getElementById("create-theater-seats").value = "";
-    const postResponse = await fetch(url, makeOptions("POST", newTheater, true)).then(handleHttpErrors)
-    console.log(postResponse)
 }
