a8a8c622d79fea6f79a20db81cef9706f239c129
simo75m1 <simo75m1@stud.kea.dk>
2023-10-13 09:26:19 +0200

    search functional except movie data

--- pages/manageReservations/reservations.js
+++ pages/manageReservations/reservations.js
@@ -4,26 +4,31 @@ const url = API_URL+"/reservations";
 
 let reservationList = [];
 
-export function initReservations(){
+export async function initReservations(){
     fetchReservations()
     document.getElementById("phone-search-btn").addEventListener("click", createReservationTable)
     document.getElementById("reset-search-btn").addEventListener("click", resetSearch)
 }
 
 
-async function createReservationTable(){
-    const filteredList = filterByPhone()
-    try{
-        const tableData = await Promise.all(filteredList.map(async (reservation) => {
-            let movie, showing;
-            try{
-                showing = await fetch(API_URL+"/showings/"+reservation.showingId).then(handleHttpErrors);
-                movie = await fetch(API_URL+"/movies/"+showing.movieId).then(handleHttpErrors);
-    }catch(error){
-        console.error(error)
+function createReservationTable(){
+    let filteredList = filterByPhone()
+    if(filteredList == null){
+        return
     }
+    try{
+        const tableData = filteredList.map((reservation) => {
+            const showingUrl = API_URL+"/showings/"+reservation.showingId;
+                let showing = fetch(showingUrl).then(handleHttpErrors).then(data => {
+                    return data;
+                });
+                const movieUrl = API_URL+"/movies/"+showing.movieId;
+                let movie = fetch(movieUrl).then(handleHttpErrors).then(data => {
+                    return data;
+                });
+    
         const theaterId = reservation.seats[0].theaterId;
-        const seatPositions = reservation.seats.map(seat => `Row ${seat.rowNumber}, Seat ${seat.seatNumber}`).join(',\n ');
+        const seatPositions = reservation.seats.map(seat => `Row ${seat.rowNumber}, Seat ${seat.seatNumber}`).join('<br> ');
 
         return `<tr>
                 <td><p>${reservation.customerInfo.phoneNumber}</p></td>
@@ -35,7 +40,8 @@ async function createReservationTable(){
                 <td><p>${seatPositions}</p></td>
                 <td><button>Delete</button></td>
                 </tr>`
-        }))
+        }).join("");
+        document.getElementById("table-rows").innerHTML = tableData;
     }catch(error){
         console.error(error)
     }
@@ -52,6 +58,24 @@ async function fetchReservations(){
     }catch(error){
         console.error(error);
     }
+}
+
+function filterByPhone(){
+    document.getElementById("search-prompt").innerText = "";
+    const phoneNumber = document.getElementById("phonenumber-input").value;
+    if(!phoneNumber){
+        resetSearch();
+    }
+    const filteredReservations = reservationList.filter(reservation => phoneNumber === reservation.customerInfo.phoneNumber);
+    if(filteredReservations){
+        return filteredReservations;
+    }
+    return null;
+}
+
+function resetSearch(){
+    document.getElementById("search-prompt").innerText = "Search for a phone number to find reservations"
+    document.getElementById("phonenumber-input").value = "";
 }
 
 // async function fetchShowing(showId){
@@ -71,18 +95,3 @@ async function fetchReservations(){
 //         console.error(error)
 //     }
 // }
-
-function filterByPhone(){
-    document.getElementById("search-prompt").innerText = "";
-    const phoneNumber = document.getElementById("phonenumber-input").value;
-    if(!phoneNumber){
-        resetSearch();
-    }
-    const filteredReservations = reservationList.filter(reservation => phoneNumber === reservation.customerInfo.phoneNumber);
-    return filteredReservations;
-}
-
-function resetSearch(){
-    document.getElementById("search-prompt").innerText = "Search for a phone number to find reservations"
-    document.getElementById("phonenumber-input").value = "";
-}
